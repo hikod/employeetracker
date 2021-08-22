73d41a34360dc106fad9bc48f68e3bb9483b4317
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-08-22 00:32:07 -0400

    adding seeds and schemas for the db

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (name)
+VALUES ("Marketing"), ("Human Resources"), ("Sales"), ("Operations") ("Finance") ("Technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 175000.00, 1),('Vice President', 350000.00, 2),
+('President', 2000000.00, 3),('CIO', 280000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1 , NULL), ('Jane', 'Doe', 2, 1),
+('Steve', 'Jobs', 3, NULL), ('Ian', 'Sharp', 4, 2);
