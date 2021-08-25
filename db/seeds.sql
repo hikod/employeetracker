741ea0dd3987eeaf0eb0e339f45005a2af558009
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-08-25 05:56:27 -0400

    adding view all employees and adding a new role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO employee_db.department (name)
 VALUES 
 ("Marketing"), 
 ("Human Resources"), 
@@ -7,18 +7,18 @@ VALUES
 ("Finance"),
 ("Technology");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO employee_db.role (title, salary, department_id)
 VALUES 
 ('Marketing Manager', 175000.00, 1),
 ('HR', 350000.00, 2),
-('President', 2000000.00, 4),
 ('Sales Manager', 380000.00, 3),
+('President', 2000000.00, 4),
 ('Finance Manager', 280000.00, 5),
 ('Software Engineer', 180000.00, 6);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee_db.employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ('John', 'Doe', 1 , 1), 
-('Jane', 'Doe', 2, 4),
-('Steve', 'Jobs', 3, 5),
+('Jane', 'Bowe', 2, 4),
+('Steve', 'Star', 3, 5),
 ('Ian', 'Sharp', 4, NULL);
